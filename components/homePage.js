b8fe37d215f414d3c58614e1b9e3e16d411fe156
Tommy Lusun <tommy.t.lusun@ibm.com>
2019-01-05 18:39:51 -0500

    update animations on homePage

--- components/homePage.js
+++ components/homePage.js
@@ -22,77 +22,14 @@ class HomePage extends Component {
             delay: 300,
             translateY: {
                 value: [-500, 0]
-            },
+            }
             // easing: [.91,-0.25,.29,1.56]
         });
-        document.addEventListener('aos:in:second-card2', ({ detail }) => {
-            console.log('animated in', detail);
-            let ht = window.innerHeight + 'px';
-		    let wt = window.innerWidth + 'px';
-            this.setState({firstTrigger: !this.state.firstTrigger});
-            if (this.state.firstTrigger){
-                anime({
-                    targets: '.first-card',
-                    translateY: [0,-700],
-                    easing: 'easeInOutQuad'
-                });
-                // anime({
-                //     targets: '.background',
-                //     scaleY: [0,1],
-                // });
-            } else {
-                anime({
-                    targets: '.first-card',
-                    translateY: [-700,0],
-                    easing: 'easeInOutQuad'
-                });
-                // anime({
-                //     targets: '.background',
-                //     scaleY: [1,0],
-                //     easing: 'easeInOutQuad'
-                // });
-            }
-        });
-        document.addEventListener('aos:in:fourth-card', ({ detail }) => {
-            console.log('animated in', detail);
-            let ht = window.innerHeight + 'px';
-		    let wt = window.innerWidth + 'px';
-            this.setState({secondTrigger: !this.state.secondTrigger});
-            if (this.state.secondTrigger){
-                anime({
-                    targets: '.background2',
-                    opacity: 1,
-                    easing: 'easeInOutQuad'
-                });
-                // anime({
-                //     targets: '.background',
-                //     scaleY: [0,1],
-                // });
-            } else {
-                anime({
-                    targets: '.background2',
-                    opacity: 0,
-                    easing: 'easeInOutQuad'
-                });
-                // anime({
-                //     targets: '.background',
-                //     scaleY: [1,0],
-                //     easing: 'easeInOutQuad'
-                // });
-            }
-        });
-        
-        
-        
-        
-
     }
     componentWillReceiveProps (){ 
         this.aos.refresh(); 
     } 
     componentWillUnmount() {
-        document.removeEventListener('aos:in:second-card2',()=>console.log('removed'));
-        // document.removeEventListener('aos:out:second-card',()=>console.log('removed'));
     }
 
     render() {
@@ -100,27 +37,41 @@ class HomePage extends Component {
         return (
         <div className="title-card title">
             <span className="background"></span>
-            <span className="background2"></span>
-            <div className="full-card first-card" >
-                {/* <svg className="welcome" style={{top: '30vh'}} width="428" height="428" viewBox="0 0 128 128">
-                        <polygon points="64 128 8.574 96 8.574 32 64 0 119.426 32 119.426 96"></polygon>
-                </svg> */}
+            {/* <span data-aos-duration="1000" data-aos-delay="100" data-aos="new-animation2" data-aos-anchor=".fourth-card" className="background2"></span> */}
+            <div data-aos-id="second-card2" 
+                className="header-text" 
+                data-aos-offset="150" 
+                data-aos-delay="100" 
+                data-aos-anchor-placement="top-top" 
+                data-aos-duration="1000"  
+                data-aos="new-animation" 
+                className="full-card first-card" >
+                
                 <div id="first-text" className="header-text" >Hi, I'm a developer</div>
                 {/* <div className="text">(but not a designer) </div> */}
                 <div className="bottom-text"> <span className="down-arrow"></span></div>
             </div>
             <div className="full-card second-card">
-                <div data-aos-id="second-card2" className="header-text" data-aos-offset="100" data-aos-anchor-placement="top-bottom" data-aos="fade-left">Scroll down..</div>
+                <div id="second-text" data-aos-id="second-card2" className="header-text text2" data-aos-offset="100" data-aos-anchor-placement="top-bottom" data-aos="fade-left">Scroll down..</div>
             </div>
 
             <div className="full-card third-card">
                 <div data-aos-id="third-card" className="header-text" data-aos-offset="100" data-aos-anchor-placement="top-bottom" data-aos="fade-left">for a special message</div>
             </div>
             <div className="full-card fourth-card">
-                <div data-aos-id="fourth-card" className="header-text"  data-aos-offset="-300" data-aos-anchor-placement="center-center" data-aos-delay="300" data-aos="fade-in">Check out my blog</div>
+                <div data-aos-id="fourth-card" className="header-text"  data-aos-offset="-300" data-aos-anchor-placement="center-center" data-aos-delay="300" data-aos="fade-in">
+                Check out my <a href="/projects">projects</a>
+                </div>
             </div>
             
         <style jsx>{`
+        [data-aos="new-animation"] {
+            transform: translateY(0px);
+            transition-property: transform, opacity;
+            }
+        [data-aos="new-animation"].aos-animate {
+            transform: translateY(-100vh);
+        }
         .background {
             position:fixed;
             left: 0;
@@ -129,16 +80,6 @@ class HomePage extends Component {
             height: 100%;
             z-index: -1;
             background-image: linear-gradient(to right, #ed6ea0 0%, #ec8c69 100%);
-        }
-        .background2 {
-            position:fixed;
-            left: 0;
-            top: 0;
-            width: 100%;
-            height: 100%;
-            z-index: -1;
-            opacity: 0;
-            background-image: linear-gradient(to right, #6a11cb 0%, #2575fc 100%);
         }
         .full-card {
             display: flex;
@@ -179,13 +120,40 @@ class HomePage extends Component {
         .third-card {
             //background: linear-gradient(180deg, #BE90D4 0%, #BE90D4 35%, #1F4788 100%);
             height: 100vh;
-
-            
         }
         .fourth-card {
             //background: linear-gradient(180deg, #1F4788 0%, #1F4788 35%, #264348 100%);
             color: white;
             height: 80vh;
+        }
+        .fourth-card a{
+            display: inline-block;
+            border: 3px solid black;
+            padding-bottom: 10px;
+            text-decoration: none;
+            color: #DDDDDD;
+            transition-duration: 0.5s;
+        }
+        
+        .fourth-card a:hover{
+            transform: scale(1.05);
+            transition-duration: 0.5s;
+            box-shadow: 0px 20px 45px -9px rgba(0,0,0,0.4);
+        }
+        .fourth-card a::before{
+            content: "";
+            position: absolute;
+            top: 0;
+            right: 0;
+            left: 0;
+            bottom: 0;
+            background: linear-gradient(to right, #6a11cb 0%, #2575fc 100%);
+            transition: 0.5s;
+            opacity: 0;
+            z-index: -1;
+        }
+        .fourth-card a:hover::before{
+            opacity: 1;
         }
         .bottom-text {
             padding: 30px;
